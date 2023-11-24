3afe0a80b5bafae9ae38d433347fd568eede9c31
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-11-24 17:00:24 -0500

    added instructors.js file

---
+++ instructors.js
@@ -0,0 +1,27 @@
+const instructorWithLongestName = function(instructors) {
+  let longestName = instructors[0];
+
+  for (const instructor of instructors) {
+    if (instructor.name.length > longestName.name.length) {
+      longestName = instructor;
+    }
+  }
+  return longestName;
+};
+
+console.log(
+  instructorWithLongestName([
+    { name: "Samuel", course: "iOS" },
+    { name: "Jeremiah", course: "Web" },
+    { name: "Ophilia", course: "Web" },
+    { name: "Donald", course: "Web" },
+  ])
+);
+
+console.log(
+  instructorWithLongestName([
+    { name: "Matthew", course: "Web" },
+    { name: "David", course: "iOS" },
+    { name: "Domascus", course: "Web" },
+  ])
+);
